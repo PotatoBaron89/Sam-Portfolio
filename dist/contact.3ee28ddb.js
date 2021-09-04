6918d7fe6fbee48e903e562243794431a3adfd31
Sam <samboy29@gmail.com>
2021-09-04 23:34:22 +1000

    fixed bug on header, cleaned html, added content to about me

---
+++ dist/contact.3ee28ddb.js
@@ -0,0 +1,2 @@
+const menuWrapper=document.getElementById("menu-wrapper-main");function showMenu(){"0"===menuWrapper.style.opacity?(menuWrapper.style.opacity="1",menuWrapper.style.width="26rem",menuWrapper.style.transform="scaleY(1)"):(menuWrapper.style.opacity="0",menuWrapper.style.width="0",menuWrapper.style.transform="scaleY(0)")}
+//# sourceMappingURL=contact.3ee28ddb.js.map
